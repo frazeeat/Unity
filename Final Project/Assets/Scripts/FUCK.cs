59fa0d28f5a3dee13412f5815cef50441d6a5f8f
Drew Brown <brownaj3@miamioh.edu>
2016-05-10 01:41:51 -0400

    better networking, but still much more to do
    units can now spawn on one side of the server and be controlled. Do not
    show up on other server at all

---
+++ Final Project/Assets/Scripts/FUCK.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.Networking;
+using System.Collections;
+
+public class FUCK : NetworkBehaviour {
+
+    public GameObject g;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if (Input.GetKeyDown("h")){
+            CmdSpawn();
+        }
+	}
+
+    [Command]
+    public void CmdSpawn()
+    {
+        GameObject go = (GameObject)Instantiate(g) as GameObject;
+        NetworkServer.SpawnWithClientAuthority(go, connectionToClient);
+    }
+}
