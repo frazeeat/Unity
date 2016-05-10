a06e4f2df0530754c3b6700e5f15f0ae18048670
unknown <frazeeat@CEC-BEN016-D4.it.muohio.edu>
2016-05-10 16:11:52 -0400

    Started on a new path for networking

---
+++ Final Project/Assets/Scripts/MatchMaker.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MatchMaker : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+	}
+	
+	// Update is called once per frame
+    void onGUI()
+    {
+        GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
+    }
+}
