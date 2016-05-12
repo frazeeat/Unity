a42969aadde202beb7738bcf743ad52c7fbf3c99
Adam Frazee <frazeeat@miamioh.edu>
2016-05-12 09:47:13 -0400

    Made a loading screen until two players join.

--- Final Project/Assets/Scripts/MatchMaker.cs
+++ Final Project/Assets/Scripts/MatchMaker.cs
@@ -4,14 +4,27 @@ using Photon;
 
 public class MatchMaker : Photon.PunBehaviour
 {
+    private bool spawned = false;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         PhotonNetwork.ConnectUsingSettings("0.1");
+        
         //PhotonNetwork.logLevel = PhotonLogLevel.Full;
     }
-	
-	// Update is called once per frame
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!spawned)
+        {
+            if (PhotonNetwork.playerList.Length == 2)
+            {
+                spawnPlayer();
+                spawned = true;
+            }
+        }
+    }
     void onGUI()
     {
         GUILayout.Label(PhotonNetwork.connectionStateDetailed.ToString());
@@ -23,22 +36,32 @@ public class MatchMaker : Photon.PunBehaviour
     }
     void OnPhotonRandomJoinFailed()
     {
+        RoomOptions room = new RoomOptions();
+        room.maxPlayers = 2;
         Debug.Log("Can't join random room!");
-        PhotonNetwork.CreateRoom(null);
+        PhotonNetwork.CreateRoom(null,room, TypedLobby.Default);
     }
     public override void OnJoinedRoom()
     {
-        Quaternion rot= Quaternion.identity;
-        rot.eulerAngles = new Vector3(90, 0, 0);
-
-        GameObject camera = PhotonNetwork.Instantiate("Main Camera", new Vector3(0,20,0), rot, 0);
-        //camera.transform.Rotate(-Vector3.left, 90.0f);
-        //camera.transform.position = new Vector3(0, 20, 0);
-        camera.GetComponent<Camera>().enabled = true;
-
-        GameObject monster = PhotonNetwork.Instantiate("Unit", Vector3.zero, Quaternion.identity, 0);
-        monster.GetComponent<MoveUnit>().enabled = true;
+        if (PhotonNetwork.playerList.Length == 2)
+        {
+            spawnPlayer();
+            turnOffCam();
+        }
 
         Debug.Log("On joined Room ran");
+    }
+    void spawnPlayer()
+    {
+        Quaternion rot = Quaternion.identity;
+        rot.eulerAngles = new Vector3(90, 0, 0);
+
+        GameObject camera = PhotonNetwork.Instantiate("Main Camera", new Vector3(0, 20, 0), rot, 0);
+        camera.GetComponent<Camera>().enabled = true;
+    }
+    [PunRPC]
+    void turnOffCam()
+    {
+        GameObject.FindGameObjectWithTag("loadingcam").SetActive(false);
     }
 }
